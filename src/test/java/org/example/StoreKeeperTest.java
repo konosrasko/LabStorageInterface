19066068b6482c424f7df0934764a46cc9a8fb15
tvasiliadis <tvasiliadis@ots.gr>
2023-04-13 13:53:14 +0300

    Commit

--- src/test/java/org/example/StoreKeeperTest.java
+++ src/test/java/org/example/StoreKeeperTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StoreKeeperTest {
     StoreKeeper st1 = new StoreKeeper();
     Slip s1 = new Slip();
+    Slip s2 = new Slip();
 
     @Test
     void setSlipList() {
@@ -15,6 +16,11 @@ class StoreKeeperTest {
         s1.addProductList(3,ProductType.FIFO,"test","kg",2);
 
         st1.setSlipList(s1);
+
+        s2.addProductList(4,ProductType.FIFO,"test","kg",2);
+        s2.addProductList(5,ProductType.FIFO,"test","kg",2);
+        s2.addProductList(6,ProductType.FIFO,"test","kg",2);
+        st1.setSlipList(s2);
         System.out.println(st1);
 
     }
