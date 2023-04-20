e24fabd519d5a82a7877f94b2d74844c7cb109ed
kraskopoulos <kraskopoulos@ots.gr>
2023-04-20 10:51:29 +0300

    emptySpot

--- src/test/java/org/example/StorageTest.java
+++ src/test/java/org/example/StorageTest.java
@@ -33,17 +33,17 @@ class StorageTest {
         s1.addProductList(0,ProductType.LIFO,"test","kilos",10);
 
 
-        storage.addSpots(0,0,0,40);
+        storage.addSpots(0,0,0,1);
         storage.addSpots(0,0,0,30);
         storage.addSpots(0,0,0,10);
         storage.addSpots(0,0,0,20);
-        st1.assignProduct(1,1,storage.getSpotsList());
-        st1.assignProduct(2,2,storage.getSpotsList());
-        st1.assignProduct(3,3,storage.getSpotsList());
+        st1.assignProduct(10,1,storage.getSpotsList());
+        st1.assignProduct(20,2,storage.getSpotsList());
+        st1.assignProduct(30,3,storage.getSpotsList());
         
 
 
-        assertEquals(0,storage.emptySpots());
+        assertEquals(1,storage.emptySpots());
 
 
     }
