5e31d156d22fb274ede977f6212307e384b16846
kraskopoulos <kraskopoulos@ots.gr>
2023-04-21 14:01:13 +0300

    Search,Exit

---
+++ src/test/java/org/example/ExitTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExitTest {
+    @Test
+    void ExitTest() {
+        Storage storage = new Storage();
+        Spot spot = new Spot();
+
+        storage.addSpots(1, 2, 3);
+        storage.addSpots(2, 2, 3);
+        storage.addSpots(3, 2, 3);
+        storage.addSpots(4, 2, 3);
+
+        EntrySlip entrySlip = new EntrySlip();
+        Search search = new Search();
+        StoreKeeper storeKeeper = new StoreKeeper();
+
+        entrySlip.addProduct(1, ProductCategory.LIFO, "test");
+        entrySlip.addProduct(2, ProductCategory.LIFO, "test");
+        entrySlip.addProduct(3, ProductCategory.LIFO, "test");
+
+
+        storage.addStoreKeeper(1, "test");
+        storage.addStoreKeeper(2, "test");
+        storage.addStoreKeeper(3, "test");
+        storage.addStoreKeeper(4, "test");
+
+        storeKeeper.addSlip(entrySlip);
+
+        Exit exit = new Exit(1,"exitTest");
+
+        exit.exitSlip(1,storage.getSpotsList());
+        assertEquals(0,storage.getStorageID());
+
+    }
+
+}
---
+++ src/test/java/org/example/SearchTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchTest {
+    @Test
+    void searchTest(){
+        Storage storage = new Storage();
+
+        storage.addSpots(1,2,3);
+        storage.addSpots(2,2,3);
+        storage.addSpots(3,2,3);
+        storage.addSpots(4,2,3);
+
+        EntrySlip entrySlip = new EntrySlip();
+        Search search = new Search();
+        StoreKeeper storeKeeper = new StoreKeeper();
+
+        entrySlip.addProduct(1,ProductCategory.LIFO,"test");
+        entrySlip.addProduct(2,ProductCategory.LIFO,"test");
+        entrySlip.addProduct(3,ProductCategory.LIFO,"test");
+
+
+        storage.addStoreKeeper(1,"test");
+        storage.addStoreKeeper(1,"test");
+        storage.addStoreKeeper(1,"test");
+        storage.addStoreKeeper(1,"test");
+
+        storeKeeper.addSlip(entrySlip);
+
+        search.searchOfProduct(1,storage.getStoreKeeperList(),storeKeeper.getSlipList());
+        assertEquals("test",search.searchOfProduct(1,storage.getStoreKeeperList(),storeKeeper.getSlipList()));
+
+    }
+
+}
