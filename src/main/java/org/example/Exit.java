67937fd556b1e4d97c8588ba82b2bb0a5b9af66b
tvasiliadis <tvasiliadis@ots.gr>
2023-04-24 12:58:18 +0300

    Exit

--- src/main/java/org/example/Exit.java
+++ src/main/java/org/example/Exit.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.example.ProductCategory.FIFO;
 
@@ -11,7 +12,7 @@ public class Exit {
     private int ExitSlip;
     private String ExitDesc;
     private LocalTime exitDate;
-    List exitSlipList = new ArrayList();
+    private List<Exit> exitSlipList = new ArrayList();
 
     public Exit(int ExitSlip, String ExitDesc){
         this.ExitSlip=ExitSlip;
@@ -21,32 +22,44 @@ public class Exit {
 
     public List exitSlip(int pid, @NotNull List<Spot> spotList, @NotNull List<Product> productList){
 
-        if (productList.get(pid).getProductGeneralType().equals(FIFO)){
 
-
-            String category = productList.get(pid).getCategory();
-            String type = productList.get(pid).getProductGeneralType();
-            Collections.sort(, Comparator.comparing(Date::getDate));
+        String category = productList.get(pid).getCategory();
+//        String type = productList.get(pid).getProductGeneralType();
+//        Collections.sort(productList,Comparator.comparing(Product::getProductDate));
 
 
 
-            for(Spot spot : spotList){
-                if (spot.getProductId()==pid){
-                exitSlipList.add(new Exit(spot.getProductId(),ExitDesc));
-                spot.setProductId(0);
+            if (productList.get(pid).getProductGeneralType().equals(FIFO)){
 
+                productList.stream()
+                        .filter(product -> product.getCategory().equals(category))
+                        .sorted(Comparator.comparing(Product::getProductDate).reversed())
+                        .collect(Collectors.toList());
+
+                for (Spot spot: spotList){
+                    if (spot.getProductId() == pid){
+                        productList.remove(productList.get(pid).getProductId());
+                        exitSlipList.add(new Exit(spot.getProductId(),ExitDesc));
+                        spot.setProductId(0);
+                    }
+                }
+
+
+            }else {
+                productList.stream()
+                        .filter(product -> product.getCategory().equals(category))
+                        .sorted(Comparator.comparing(Product::getProductDate))
+                        .collect(Collectors.toList());
+
+                for (Spot spot: spotList){
+                    if (spot.getProductId() == pid){
+                        productList.remove(productList.get(pid).getProductId());
+                        exitSlipList.add(new Exit(spot.getProductId(),ExitDesc));
+                        spot.setProductId(0);
+                    }
                 }
             }
 
-        }else {
-            for(Spot spot : spotList){
-                if (spot.getProductId()==pid){
-                    exitSlipList.add(new Exit(spot.getProductId(),ExitDesc));
-                    spot.setProductId(0);
-
-                }
-            }
-        }
 
         return exitSlipList;
     }
@@ -57,11 +70,4 @@ public class Exit {
     }
 
 
-
-    public void exitProduct(){
-        Collections.sort(exitSlipList);
-        for (Object exit : exitSlipList) {
-            System.out.println(exit);
-        }
-    }
 }
