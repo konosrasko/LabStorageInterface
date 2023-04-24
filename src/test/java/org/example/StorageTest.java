2563b8a0c1ed9b097c0947a60713b113a13c08f7
kraskopoulos <kraskopoulos@ots.gr>
2023-04-24 09:55:42 +0300

    Search,Exit

--- src/test/java/org/example/StorageTest.java
+++ src/test/java/org/example/StorageTest.java
@@ -1,13 +1,8 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class StorageTest1 {
+class StorageTest {
     Storage s1 = new Storage();
     @Test
     void storage(){
