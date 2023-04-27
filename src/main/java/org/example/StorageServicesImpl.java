0e1d0ff07dfa3595e999b0e43aab3d17e09e9970
konosrasko <konosrasko98@gmail.com>
2023-04-27 17:49:06 +0300

    Exceptions

--- src/main/java/org/example/StorageServicesImpl.java
+++ src/main/java/org/example/StorageServicesImpl.java
@@ -11,27 +11,44 @@ public class StorageServicesImpl implements StorageServices{
 
     @Override
     public int findSpot(@NotNull Storage storage, int productId) {
+        if (productId >= storage.getSpotList().size() || productId < 0) {
+            throw new IndexOutOfBoundsException("Invalid product ID: " + productId);
+        }
         return storage.getSpotList().get(productId).getSpotID();
     }
-
     @Override
     public void entrySlip(@NotNull Storage storage, EntrySlip slip, int storekeeperId) {
+        if (slip == null) {
+            throw new NullPointerException("Entry slip cannot be null");
+        }
         storage.getStoreKeeperList().get(storekeeperId).addSlip(slip);
         storage.getSortedProductList().addAll(slip.getProductList());
     }
 
-
     @Override
     public void assign(@NotNull Storage storage, int slipId, int storeKeeperId) {
-        for (Product product : storage.getStoreKeeperList().get(storeKeeperId).getEntrySlipList().get(slipId).getProductList() ){
-            for (Spot spot : storage.getSpotList()){
-                if (spot.getProductId() == 0 ){
+        List<Product> productList = storage.getStoreKeeperList().get(storeKeeperId).getEntrySlipList().get(slipId).getProductList();
+        List<Spot> spotList = storage.getSpotList();
+
+
+        if (spotList == null || spotList.isEmpty()) {
+            throw new RuntimeException("No spots available to assign products to");
+        }
+
+
+        for (Product product : productList) {
+            boolean assigned = false;
+            for (Spot spot : spotList) {
+                if (spot.getProductId() == 0) {
                     spot.setProductId(product.getProductId());
+                    assigned = true;
                     break;
                 }
             }
+            if (!assigned) {
+                throw new RuntimeException("No spots available to assign products to");
+            }
         }
-        //clear
     }
 
     @Override
